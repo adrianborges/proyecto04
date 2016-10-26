150d3ef402a78dcb13c4c5547b75d8c1a0e30e04
armandokiter <armandokiter@armando>
2016-10-26 21:54:51 +0200

    hola mundo

--- holamundo/src/holamundo/holamundo.java
+++ holamundo/src/holamundo/holamundo.java
@@ -4,7 +4,8 @@ public class holamundo {
 
 	public static void main(String[] args) {
 		// imprimir datos en pantalla
+		{System.out.println("Hola Mundo");}
 
-	System.out.println ("hola mundo");}
+	}
 
 }
